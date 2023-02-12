0968e526dc170cb1fe4fd9fad78e921286a71067
Nikita Alexov <polarbeararmor@gmail.com>
2023-02-12 13:47:53 +0300

    update ui

--- src/components/WeatherCitiesList.vue
+++ src/components/WeatherCitiesList.vue
@@ -1,25 +1,25 @@
 <template>
-  <div class="mt-3 bg-slate-300 rounded-2xl">
+  <div class="mt-5 bg-slate-300 rounded-2xl">
     <div
-      class="p-2 flex items-center justify-between bg-slate-50 rounded-xl group hover:bg-slate-400 transition-colors [&:not(:last-child)]:mb-2"
-      v-for="(city, idx) in cities" :key="city.id"
-      draggable="true"
-      @dragstart="onDragStart($event, idx, city)"
-      @drop="onDrop($event, idx)"
-      @dragover.prevent
-      @dragenter.prevent
+      class="max-h-[18rem] overflow-y-auto scrollbar"
+      :class="cities.length > 6 ? 'pr-[1rem]' : ''"
     >
-      <div>
-        <span class="block w-5 h-5 cursor-grab active:cursor-grabbing">
-          <Bars3Icon class="w-full h-full group-hover:text-white transition-colors" />
+      <div
+        class="p-2 flex items-center justify-between bg-slate-50 rounded-xl group hover:bg-slate-400 transition-colors [&:not(:last-child)]:mb-2"
+        v-for="(city, idx) in cities" :key="city.id" draggable="true" @dragstart="onDragStart($event, idx, city)"
+        @drop="onDrop($event, idx)" @dragover.prevent @dragenter.prevent>
+        <div>
+          <span class="block w-5 h-5 cursor-grab active:cursor-grabbing">
+            <Bars3Icon class="w-full h-full group-hover:text-white transition-colors" />
+          </span>
+        </div>
+        <span class="max-w-[14rem] truncate group-hover:text-white transition-colors">
+          {{ city.name }}
         </span>
+        <button class="w-5 h-5 group-hover:text-red-700 transition-colors" @click="$emit('deleteItem', idx)">
+          <TrashIcon class="w-full h-full" />
+        </button>
       </div>
-      <span class="max-w-[14rem] truncate group-hover:text-white transition-colors">
-        {{ city.name }}
-      </span>
-      <button class="w-5 h-5 group-hover:text-red-700 transition-colors" @click="$emit('deleteItem', idx)">
-        <TrashIcon class="w-full h-full" />
-      </button>
     </div>
   </div>
 </template>
